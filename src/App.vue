1bfa02be293ae9c072f9316e1f3ad00acf9204e4
alfianchii <pianaee379@gmail.com>
2024-05-24 17:11:44 +0700

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+	<Hello name="Alfian" />
+</template>
+
+<style>
+html {
+	background: #222;
+	color: white;
+	color-scheme: dark;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		Vue(),
+		Components({
+			dts: true,
+		}),
+	],
+});
