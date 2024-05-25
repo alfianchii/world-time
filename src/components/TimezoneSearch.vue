610fca8fd1ba933d6dab8eaad08a160cfd1c8e46
alfianchii <pianaee379@gmail.com>
2024-05-25 15:40:13 +0700

    style: button list full width

--- src/components/TimezoneSearch.vue
+++ src/components/TimezoneSearch.vue
@@ -24,7 +24,7 @@ function add(t: Timezone) {
 		<input v-model="input" type="text" placeholder="Search timezone ..." px2 py1 border="~ gray/15 rounded" bg-transparent w-full />
 
 		<div absolute top-full bg-gray-900 left-0 right-0 v-show="input">
-			<button @click="add(i.item)" v-for="i of searchResult" :key="i.refIndex" flex gap2>
+			<button @click="add(i.item)" v-for="i of searchResult" :key="i.refIndex" flex gap2 w-full>
 				<div font-mono w-10 text-right>
 					{{ i.item.offset }}
 				</div>
